de80a8fb45386ce078bf5a171923f712cfb67856
yshkoropad <yshkoropad@greensightag.com>
2022-12-02 18:16:47 +0200

    add never

--- index.ts
+++ index.ts
@@ -38,3 +38,31 @@ const birdLifeSpan: BirdLifeSpan = {
   [Bird.eagle]: '10-20 years'
 };
 
+// Generic
+function identity<T>(val: T): T {
+  return val;
+}
+
+identity<number>(10);
+
+// Generic match some interface
+interface Pet {
+  name: string
+}
+
+interface Dog {
+  name: string;
+  age: number;
+}
+
+function getName<T extends Pet>(val: T): string {
+  return val.name;
+}
+
+getName<Dog>({name: 'Bark', age: 10});
+getName<Dog>({name: 'Bark', age: 10});
+
+// Function with error throw
+function throwError(): never {
+  throw Error('Data is not valid')
+}
