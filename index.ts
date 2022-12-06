38abdc9efa2b039833f4ff15b213bebc3ac8407b
yshkoropad <yshkoropad@greensightag.com>
2022-12-06 17:02:58 +0200

    add conditional arguments

--- index.ts
+++ index.ts
@@ -80,3 +80,26 @@ type LooseAutocomplete<T extends string> = T | Omit<string, T>;
 type Color = LooseAutocomplete<'Red' | 'Blue'>;
 
 const red: Color = 'Red';
+
+/*** Example 9. Conditional arguments ***/
+type LogInEvent = {
+  type: 'login',
+  payload: {
+    email: string
+  }
+}
+
+type LogOutEvent = {
+  type: 'logout'
+}
+
+type AppEvent = LogInEvent | LogOutEvent;
+
+const sendEvent = <Type extends AppEvent['type']>(
+  ...args: Extract<AppEvent, {type: Type}> extends { payload: infer TPayload }
+  ? [Type, TPayload]
+  : [Type]
+) => {}
+
+sendEvent('login', {email: 'email'})
+sendEvent('logout')
